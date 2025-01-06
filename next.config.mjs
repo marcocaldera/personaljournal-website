d75aea361249815aee6ab3c67ce8a32ed823ec93
Marco Caldera <marco.caldera93@icloud.com>
2025-01-06 22:49:10 +0100

    feat: init website

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withContentlayer } from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     unoptimized: true,
   },
