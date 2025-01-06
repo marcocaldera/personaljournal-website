2d22c83ee18ba84808f729a7f592bc7888aba3e2
Marco Caldera <marco.caldera93@icloud.com>
2025-01-06 23:09:52 +0100

    feat: init website

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,10 @@ import { withContentlayer } from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  async redirects() {
-    return [
-      {
-        source: '/sitemap',
-        destination: '/sitemap.xml',
-        permanent: true,
-      },
-      {
-        source: '/robots',
-        destination: '/robots.txt',
-        permanent: true,
-      },
-    ]
+  images: {
+    unoptimized: true,
   },
   webpack(config) {
     config.module.rules.push({
